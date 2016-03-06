1f66cd575195011f62af3cbecc23e33772430146
codectile <codectile@gmail.com>
2016-03-06 13:53:29 +0530

    new function and collision shapes

--- src/CCollision.cpp
+++ src/CCollision.cpp
@@ -1,6 +1,6 @@
 /*
 cimulator plugin for SanAndreas Multiplayer
-Copyright (c) 2015 codectile
+Copyright (c) 2016 codectile
 
 This software is provided 'as-is', without any express or implied warranty.
 In no event will the authors be held liable for any damages arising from the use of this software.
@@ -20,13 +20,14 @@ subject to the following restrictions:
 #include "CCollision.h"
 #include <vector>
 
-btCompoundShape* static_shapes;
-btCompoundShape* dynamic_shapes;
+#define SIMD_DEG_TO_RAD	 btScalar(0.01745329251994329576923690768489)
+#define SIMD_RADIAN_TO_DEG	 btScalar(57.295779513082320876798154814105)
 
 std::vector<btCollisionObject*> cache_bodies;
 
 ColData* cData;
 cINST* inst;
+TVehicle* vehicle_col;
 
 int cr_readCadb()
 {
@@ -67,9 +68,9 @@ int cr_readCadb()
 				cData[i].face = new cFaces[cData[i].numFaces];
 				for (int f = 0; f < cData[i].numFaces; f++)
 				{
-					file.read((char*)&cData[i].face[f].face1, 12);
-					file.read((char*)&cData[i].face[f].face2, 12);
-					file.read((char*)&cData[i].face[f].face3, 12);
+					file.read((char*)&cData[i].face[f].v0, 12);
+					file.read((char*)&cData[i].face[f].v1, 12);
+					file.read((char*)&cData[i].face[f].v2, 12);
 				}
 			}
 		}
@@ -88,6 +89,126 @@ int cr_readCadb()
 		return 1;
 	}
 	return 0;
+}
+
+int cr_readCCF()
+{
+	std::fstream file;
+	file.open("scriptfiles\\cimulator\\SAMPVehicleCOLs.ccf", std::fstream::in | std::fstream::binary);
+	if (file.is_open())
+	{
+		THeader header;
+		file.read(reinterpret_cast<char*>(&header), sizeof(THeader));
+		if (header.m_magic[0] == 'C' && header.m_magic[1] == 'C' && header.m_magic[2] == 'F' && header.m_magic[3] == '1')
+		{
+			vehicle_col = new TVehicle[header.m_numModels];
+
+			for (uint16_t i = 0; i < 612; i++)
+				vehicleRef[i] = 0;
+
+			for (uint16_t i = 0; i < 211; i++)
+			{
+				file.read(reinterpret_cast<char*>(&vehicle_col[i]), 8);
+				
+				vehicleRef[vehicle_col[i].m_modelid] = i;
+
+				if (vehicle_col[i].m_numspheres > 0)
+				{
+					vehicle_col[i].spheres = new cSphere[vehicle_col[i].m_numspheres];
+					for (int s = 0; s < vehicle_col[i].m_numspheres; s++)
+					{
+						file.read(reinterpret_cast<char*>(&vehicle_col[i].spheres[s]), 16);
+						//file.read(reinterpret_cast<char*>(&vehicle_col[i].spheres[s].radius), 4);
+					}
+				}
+
+				if (vehicle_col[i].m_numboxes > 0)
+				{
+					vehicle_col[i].boxes = new cBox[vehicle_col[i].m_numboxes];
+					for (int b = 0; b < vehicle_col[i].m_numboxes; b++)
+					{
+						file.read(reinterpret_cast<char*>(&vehicle_col[i].boxes[b]), 24);
+					}
+				}
+
+				if (vehicle_col[i].m_numfaces > 0)
+				{
+					vehicle_col[i].faces = new cFaces[vehicle_col[i].m_numfaces];
+					for (int f = 0; f < vehicle_col[i].m_numfaces; f++)
+					{
+						file.read(reinterpret_cast<char*>(&vehicle_col[i].faces[f]), 36);
+					}
+				}
+			}
+			file.close();
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void cr_createColVehicle()
+{
+	vehicle_shapes = new btCompoundShape[211];
+	btQuaternion zero(0, 0, 0, 1);
+	for (int i = 0; i < 211; i++)
+	{
+		if (vehicle_col[i].m_numspheres > 0)
+		{
+			for (int s = 0; s < vehicle_col[i].m_numspheres; s++)
+			{
+				btSphereShape* sphere = new btSphereShape(vehicle_col[i].spheres[s].radius);
+				vehicle_shapes[i].addChildShape(btTransform(zero, btVector3(vehicle_col[i].spheres[s].center.x, vehicle_col[i].spheres[s].center.y, vehicle_col[i].spheres[s].center.z)), sphere);
+			}
+		}
+
+		if (vehicle_col[i].m_numboxes > 0)
+		{
+			for (int b = 0; b < vehicle_col[i].m_numboxes; b++)
+			{
+				btBoxShape* box = new btBoxShape(btVector3(vehicle_col[i].boxes[b].size.x, vehicle_col[i].boxes[b].size.y, vehicle_col[i].boxes[b].size.z));
+				vehicle_shapes[i].addChildShape(btTransform(zero, btVector3(vehicle_col[i].boxes[b].center.x, vehicle_col[i].boxes[b].center.y, vehicle_col[i].boxes[b].center.z)), box);
+			}
+		}
+
+		if (vehicle_col[i].m_numfaces > 0)
+		{
+			btTriangleMesh* trimesh = new btTriangleMesh();
+			btConvexTriangleMeshShape* convex;
+			for (int f = 0; f < vehicle_col[i].m_numfaces; f++)
+			{
+				trimesh->addTriangle(btVector3(vehicle_col[i].faces[f].v0.x, vehicle_col[i].faces[f].v0.y, vehicle_col[i].faces[f].v0.z),
+					btVector3(vehicle_col[i].faces[f].v1.x, vehicle_col[i].faces[f].v1.y, vehicle_col[i].faces[f].v1.z),
+					btVector3(vehicle_col[i].faces[f].v2.x, vehicle_col[i].faces[f].v2.y, vehicle_col[i].faces[f].v2.z));
+			}
+
+			convex = new btConvexTriangleMeshShape(trimesh);
+			vehicle_shapes[i].addChildShape(btTransform(zero, btVector3(0, 0, 0)), convex);
+		}
+	}
+
+	delete[] vehicle_col->spheres;
+	delete[] vehicle_col->boxes;
+	delete[] vehicle_col->faces;
+	delete[] vehicle_col;
+}
+
+btCollisionObject* cr_createVehicleCollision(btDiscreteDynamicsWorld* dynamicsWorld, int modelid, btVector3& position, btQuaternion& rotation)
+{
+	int index = vehicleRef[modelid];
+	btCollisionObject* collision = new btCollisionObject();
+	collision->setCollisionShape((btCompoundShape*)(&vehicle_shapes[index]));
+	collision->setCollisionFlags(1);
+	collision->setWorldTransform(btTransform(rotation, position));
+	collision->setActivationState(ACTIVE_TAG);
+	collision->setUserIndex(modelid);
+	dynamicsWorld->addCollisionObject(collision);
+	return collision;
+}
+
+void cr_removeVehicleCol(btDiscreteDynamicsWorld* dynamicsWorld, btCollisionObject* col)
+{
+	dynamicsWorld->removeCollisionObject(col);
 }
 
 void cr_createColObject()
@@ -124,9 +245,9 @@ void cr_createColObject()
 			btConvexTriangleMeshShape* convex;
 			for (int f = 0; f < cData[i].numFaces; f++)
 			{
-				trimesh->addTriangle(btVector3(cData[i].face[f].face1.x, cData[i].face[f].face1.y, cData[i].face[f].face1.z),
-					btVector3(cData[i].face[f].face2.x, cData[i].face[f].face2.y, cData[i].face[f].face2.z),
-					btVector3(cData[i].face[f].face3.x, cData[i].face[f].face3.y, cData[i].face[f].face3.z));
+				trimesh->addTriangle(btVector3(cData[i].face[f].v0.x, cData[i].face[f].v0.y, cData[i].face[f].v0.z),
+					btVector3(cData[i].face[f].v1.x, cData[i].face[f].v1.y, cData[i].face[f].v1.z),
+					btVector3(cData[i].face[f].v2.x, cData[i].face[f].v2.y, cData[i].face[f].v2.z));
 			}
 			mesh = new btBvhTriangleMeshShape(trimesh, true);
 			convex = new btConvexTriangleMeshShape(trimesh);
@@ -153,7 +274,7 @@ void cr_objectPlacement(btDiscreteDynamicsWorld* dynamicsWorld, btScalar worldre
 		rigidBody->setRestitution(worldrest);
 		dynamicsWorld->addRigidBody(rigidBody);
 		cache_bodies.push_back(rigidBody);
-		
+
 		* The above code is absolutely slow and it consumes a lot of memory. A better version of code is written below
 		* which fast and efficient.
 		*/
@@ -167,24 +288,60 @@ void cr_objectPlacement(btDiscreteDynamicsWorld* dynamicsWorld, btScalar worldre
 	}
 }
 
+void cr_removeBuilding(btDiscreteDynamicsWorld* dynamicsWorld, int modelid, btScalar x, btScalar y, btScalar z, btScalar radius)
+{
+	if (!cache_bodies.empty())
+	{
+		if (modelid != -1)
+		{
+			for (uint32_t i = 0; i < cache_bodies.size(); i++)
+			{
+				if (cache_bodies[i]->getUserIndex() == modelid)
+				{
+					btVector3 pos = cache_bodies[i]->getWorldTransform().getOrigin();
+					if (pos.distance2(btVector3(x, y, z)) <= (radius * radius))
+					{
+						dynamicsWorld->removeCollisionObject(cache_bodies[i]);
+						delete cache_bodies[i];
+						cache_bodies[i] = NULL;
+					}
+				}
+			}
+		}
+		else
+		{
+			for (uint32_t i = 0; i < cache_bodies.size(); i++)
+			{
+				btVector3 pos = cache_bodies[i]->getWorldTransform().getOrigin();
+				if (pos.distance2(btVector3(x, y, z)) <= (radius * radius))
+				{
+					dynamicsWorld->removeCollisionObject(cache_bodies[i]);
+					delete cache_bodies[i];
+					cache_bodies[i] = NULL;
+				}
+			}
+		}
+	}
+}
+
 //all collision data related functions
 btCollisionObject* cr_addStaticCollision(btDiscreteDynamicsWorld* dynamicsWorld, int modelid, btVector3& position, btVector3& rotation)
 {
 	btQuaternion rot;
-	rot.setEuler(rotation.getZ(), rotation.getY(), rotation.getX());
-	btCollisionObject* col = new btCollisionObject();
-	col->setCollisionShape((btCompoundShape*)(&dynamic_shapes[modelid]));
-	col->setCollisionFlags(1);
-	col->setWorldTransform(btTransform(rot, position));
-	col->setActivationState(ACTIVE_TAG);
-	dynamicsWorld->addCollisionObject(col);
-	return col;
+	rot.setEuler(rotation.getZ() * SIMD_DEG_TO_RAD, rotation.getY() * SIMD_DEG_TO_RAD, rotation.getX() * SIMD_DEG_TO_RAD);
+	btCollisionObject* collision = new btCollisionObject();
+	collision->setCollisionShape((btCompoundShape*)(&dynamic_shapes[modelid]));
+	collision->setCollisionFlags(1);
+	collision->setWorldTransform(btTransform(rot, position));
+	collision->setActivationState(ACTIVE_TAG);
+	dynamicsWorld->addCollisionObject(collision);
+	return collision;
 }
 
 btRigidBody* cr_addDynamicCollision(btDiscreteDynamicsWorld* dynamicsWorld, int modelid, btScalar mass, btVector3& position, btVector3& rotation, int state)
 {
 	btQuaternion rot;
-	rot.setEuler(rotation.getZ(), rotation.getY(), rotation.getX());
+	rot.setEuler(rotation.getZ() * SIMD_DEG_TO_RAD, rotation.getY() * SIMD_DEG_TO_RAD, rotation.getX() * SIMD_DEG_TO_RAD);
 	btMotionState* motionState = new btDefaultMotionState(btTransform(rot, position));
 	btVector3 inertia(0, 0, 0);
 	if (mass <= 0.f)
@@ -257,7 +414,7 @@ void cr_removeRoadBlocks(btDiscreteDynamicsWorld* dynamicsWorld)
 {
 	for (unsigned int i = 0; i < cache_bodies.size(); i++)
 	{
-		if (cache_bodies[i] != NULL && inst[i].modelid == 1662 || (inst[i].modelid >= 4504 && inst[i].modelid <= 4527)) //no default road blocks will be added
+		if (cache_bodies[i] != NULL && (inst[i].modelid == 1662 || (inst[i].modelid >= 4504 && inst[i].modelid <= 4527))) //no default road blocks will be added
 		{
 			dynamicsWorld->removeCollisionObject(cache_bodies[i]);
 			delete cache_bodies[i];
